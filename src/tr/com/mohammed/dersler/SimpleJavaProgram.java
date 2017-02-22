577c8910b7b8ddc53e22569e560ad354c12892de
mohammedrbaker <mohbeyatli@gmail.com>
2017-02-22 22:45:40 +0300

    This is another test for uploading to GitHub

---
+++ src/tr/com/mohammed/dersler/SimpleJavaProgram.java
@@ -0,0 +1,19 @@
+package tr.com.mohammed.dersler;
+
+/**
+ * Created by Mohammed on 22/2/2017.
+ */
+public class SimpleJavaProgram {
+
+    public static void main(String[] args) {
+        for (int i = 0; i <=10 ; i++) {
+
+            System.out.print(i + " ");
+        }
+        System.out.println();
+        for (int i = 10; i >=0 ; i--) {
+
+            System.out.print(i + " ");
+        }
+    }
+}
