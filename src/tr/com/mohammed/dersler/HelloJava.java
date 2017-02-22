7b0078911ff0d89028e1810c729fd8b003b37b96
mohammedrbaker <mohbeyatli@gmail.com>
2017-02-22 22:31:10 +0300

    This is my H/W for Java101 course

---
+++ src/tr/com/mohammed/dersler/HelloJava.java
@@ -0,0 +1,11 @@
+package tr.com.mohammed.dersler;
+
+/**
+ * Created by Mohammed on 22/2/2017.
+ */
+public class HelloJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello Java from GitHub !");
+    }
+}
